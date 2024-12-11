d3f8bbd1c8cecf1fdafc7024adff3d172785f390
AlinValentin7 <trandafiralinvalentin@gmail.com>
2024-12-11 12:02:31 +0100

    add:first comimit

---
+++ src/test/java/it/exercise/java/spring/mvc/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.exercise.java.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
